6f25387b15a10d828c97ce63c33eb6544820df10
Kristof Dewilde <git@krivi.be>
2019-08-24 22:43:38 +0200

    Hello Feed

---
+++ README.md
@@ -0,0 +1,58 @@
+# Feednite
+
+An RSS feed generator for Fortnite news I wrote in 59 minutes, because much like [Psynix with Rocket League](https://github.com/Qrivi/RLRSS), Epic Games is not providing an RSS feed to follow up on their news updates either.
+
+The script generates a very complete and valid RSS feed on the fly and takes 3 optional parameters.
+
+* * *
+
+## Parameters
+
+| Name   | Default | Description |
+| ------ | ------- | ----------- |
+| locale | `en`    | The language in which the fetched articles should be. Falls back to English if the language is not supported on Epic's website. |
+| limit  | `25`    | The amount of recent news items to include in the feed. `-1` will include all items, and `0` will make the feed pretty useless. |
+| detail | `true`  | Whether the entire news article should be parsed into the `<description>`, or just a short description when set to `false`, `no`, `nope`, `off` or `0`. |
+
+Examples:
+
+-   [https://feednite.herokuapp.com?limit=50&detail=no](https://feednite.herokuapp.com?limit=50&detail=no)
+-   [https://feednite.herokuapp.com?limit=5&detail=1](https://feednite.herokuapp.com?limit=5&detail=1)
+-   [https://feednite.herokuapp.com?limit=420&detail=off](https://feednite.herokuapp.com?limit=420&detail=off)
+-   [https://feednite.herokuapp.com?limit=25&detail=true](https://feednite.herokuapp.com?limit=10&detail=true) (default)
+
+## Example
+
+```xml
+<?xml version="1.0" encoding="UTF-8"?>
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+  <channel>
+    <title>Fortnite News</title>
+    <description>RSS feed for Fortnite News</description>
+    <link>https://www.epicgames.com/fortnite</link>
+    <language>en</language>
+    <copyright>2019 Epic Games, Inc. - All Rights Reserved</copyright>
+    <generator>Feednite</generator>
+    <docs>https://github.com/Qrivi/Feednite</docs>
+    <image>
+      <title>Fortnite News</title>
+      <link>https://www.epicgames.com/fortnite</link>
+      <url>https://pbs.twimg.com/profile_images/1017458813199372289/QtGv1tyn_400x400.jpg</url>
+    </image>
+    <atom:link type="application/rss+xml" rel="self" href="https://local.dev:3000/?limit=1&details=nope"/>
+    <item>
+      <title>Save the World Cosmetic Update #2</title>
+      <guid>https://www.epicgames.com/fortnite/blog/save-the-world-cosmetic-update-2</guid>
+      <link>https://www.epicgames.com/fortnite/blog/save-the-world-cosmetic-update-2</link>
+      <description><![CDATA[<strong><span style="font-size:12pt;font-family:'Proxima Nova',sans-serif;white-space:pre;">Save the World Cosmetic Update #2</span></strong>]]></description>
+      <author>support@epicgames.desk-mail.com (The Fortnite Team)</author>
+      <pubDate>Fri, 23 Aug 2019 06:00:00 GMT</pubDate>
+      <category>announcements</category>
+    </item>
+  </channel>
+</rss>
+```
+
+## Disclaimer
+
+I'm not affiliated with Epic Games or Fortnite in any way: I just wanted to be able to be kept up to date with their news via my feed reader (I am a simple man). If I did anything anyone's not ok with here, please slide in my DMs before taking legal action.
---
+++ feed.js
@@ -0,0 +1,125 @@
+#!/usr/bin/env node
+const express = require('express')
+const request = require('sync-request')
+const convert = require('xml-js')
+const format = require('dateformat')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+const feedTitle = 'Fortnite News'
+const siteRoot = 'https://www.epicgames.com/fortnite'
+const postsEndpoint = '/api/blog/getPosts'
+
+const defaultLimit = 25
+const defaultLocale = 'en'
+const defaultDetails = true
+
+// Fetch news articles from Fortnite's news API
+function fetch (limit, locale, details) {
+  if (!limit) {
+    return []
+  }
+
+  const url = `${siteRoot}${postsEndpoint}?postsPerPage=${limit}&locale=${locale}&offset=0`
+  const res = request('GET', url)
+  console.log(`${format(new Date())}: Fetched posts from ${url}`)
+
+  return JSON.parse(res.body.toString()).blogList.map(post => ({
+    title: {
+      _text: post.title
+    },
+    guid: {
+      _text: `${siteRoot}${post.urlPattern}`
+    },
+    link: {
+      _text: `${siteRoot}${post.urlPattern}`
+    },
+    description: {
+      _cdata: details ? post.content : post.short
+    },
+    author: {
+      _text: `support@epicgames.desk-mail.com (${post.author})`
+    },
+    pubDate: {
+      _text: `${format(new Date(post.date), 'ddd, dd mmm yyyy HH:MM:ss')} GMT`
+    },
+    category: post.category.map(c => ({ _text: c }))
+  }))
+}
+
+// Parse news articles into an RSS 2.0 body
+function parse (posts, locale, self) {
+  return {
+    _declaration: {
+      _attributes: {
+        version: '1.0',
+        encoding: 'UTF-8'
+      }
+    },
+    rss: {
+      _attributes: {
+        version: '2.0',
+        'xmlns:atom': 'http://www.w3.org/2005/Atom'
+      },
+      channel: {
+        title: {
+          _text: feedTitle
+        },
+        description: {
+          _text: `RSS feed for ${feedTitle}`
+        },
+        link: {
+          _text: siteRoot
+        },
+        language: {
+          _text: locale
+        },
+        copyright: {
+          _text: `${format(new Date(), 'yyyy')} Epic Games, Inc. - All Rights Reserved`
+        },
+        generator: {
+          _text: 'Feednite'
+        },
+        docs: {
+          _text: 'https://github.com/Qrivi/Feednite'
+        },
+        image: {
+          title: {
+            _text: feedTitle
+          },
+          link: {
+            _text: siteRoot
+          },
+          url: {
+            _text: 'https://pbs.twimg.com/profile_images/1017458813199372289/QtGv1tyn_400x400.jpg'
+          }
+        },
+        'atom:link': {
+          _attributes: {
+            type: 'application/rss+xml',
+            rel: 'self',
+            href: self
+          }
+        },
+        item: posts
+      }
+    }
+  }
+}
+
+// Set up Express.
+app.use(express.urlencoded({ extended: true }))
+app.get('/', function (req, res) {
+  const limit = req.query.limit ? parseInt(req.query.limit) : defaultLimit
+  const locale = req.query.locale ? req.query.locale : defaultLocale
+  const details = req.query.details ? !/^(false|no(pe)?|off|0)$/i.test(req.query.details) : defaultDetails
+  const self = req.protocol + '://' + req.get('host') + req.originalUrl
+
+  res.set('Content-Type', 'application/rss+xml; charset=UTF-8')
+    .status(200)
+    .send(convert.js2xml(parse(fetch(limit, locale, details), locale, self), { compact: true, spaces: 2 }))
+})
+app.listen(port, function () {
+  console.log(`Started on port ${port}`)
+})
